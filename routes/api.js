3f207b7fc13a4ec4bbb866f788f347cd42a5f313
Darryl Masson <darrylmasson@gmail.com>
2020-02-20 14:22:46 +0100

    Work on api, time to test

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,14 @@ var router = express.Router();
 var last_api_call = {};
 const MAX_STORED_CALLS = 20;
 const MAX_TIME_FOR_CALLS = 10000; // ms
+var status_enum = [
+  "idle",
+  "arming",
+  "armed",
+  "running",
+  "error",
+  "unknown"
+];
 
 function checkKey(req, res, next) {
   var q = url.parse(req.url, true).query;
@@ -19,14 +27,15 @@ function checkKey(req, res, next) {
   var options = {"projection" : {"api_key" : 1}};
   collection.find(query, options, function(e, docs) {
     if (e) {
-      return res.send(JSON.stringify({'message' : e.message}));
+      return res.send(JSON.stringify({message : e.message}));
     }
     if (docs.length == 0 || typeof(docs[0].api_key) == 'undefined')
-      return res.send(JSON.stringify({'message' : 'Access denied'}));
+      return res.send(JSON.stringify({message : 'Access denied'}));
     bcrypt.compare(key, docs[0]['api_key'], function(err, ret) {
-      if (err) return res.send(JSON.stringify({'message' : err}));
+      if (err) return res.send(JSON.stringify({message : err}));
       if (ret == true) {
-        if (typeof(last_api_call[user]) == 'undefined') {
+        return next();
+/*        if (typeof(last_api_call[user]) == 'undefined') {
           last_api_call[user] = [];
         }
         if (last_api_call[user].length == MAX_STORED_CALLS) {
@@ -34,22 +43,23 @@ function checkKey(req, res, next) {
           last_api_call[user].push(now);
           var then = last_api_call[user].shift();
           if (Math.abs(now - then) < MAX_TIME_FOR_CALLS) {
-            return res.send(JSON.stringify({'message' : 'Too many calls'}));
+            return res.send(JSON.stringify({message : 'Chill'}));
           } else {
             return next();
           }
-        } else {
+        } else { // if number of calls
           last_api_call[user].push(new Date());
           return next();
-        }
-      }
-      return res.send(JSON.stringify({'message' : 'Access denied'}));
+        } */
+      } // if (ret == true)
+      console.log('User ' + user + ' key ' + key + ' db ' + docs[0]['api_key']);
+      return res.send(JSON.stringify({message : 'Access Denied'}));
     });
 }
 
 router.get("/helloworld", checkKey, function(req, res) {
   var today = new Date();
-  return res.send(JSON.stringify({'message' :
+  return res.send(JSON.stringify({message :
     'Hello to you too. The current time is ' + today.toUTCString()}));
 });
 
@@ -62,23 +72,23 @@ router.get("/getstatus/:host", checkKey, function(req, res) {
     time_sec = parseInt(q.time_seconds);
   } catch(error){
   }
-  var query = {"host" : host};
-  var options = {'sort' : {'_id' : -1}};
+  var query = {host : host};
+  var options = {sort : {'_id' : -1}};
   if (time_sec > 0) {
     var now = Math.floor(new Date().getTime()/1000 - time_sec);
     var oid = ObjectId(now.toString(16) + "0000000000000000");
-    query['_id'] = {'$gte' : oid};
+    query['_id'] = {$gte : oid};
   } else {
     options['limit'] = 1;
   }
 
   collection.find(query, options, function(err, docs) {
     if (err) {
-      return res.send(JSON.stringify({'message' : err.message}));
+      return res.send(JSON.stringify({message : err.message}));
     }
     return res.send(JSON.stringify(docs));
   });
-  return res.send(JSON.stringify({'message' : 'Query returned no documents'}));
+  return res.send(JSON.stringify({message : 'Query returned no documents'}));
 });
 
 router.get("/geterrors", checkKey, function(req, res) {
@@ -89,18 +99,18 @@ router.get("/geterrors", checkKey, function(req, res) {
     min_level = parseInt(q.level);
   } catch(error){
   }
-  var query = {'priority' : {'$gte' : min_level, '$lt' : 5}};
-  var options = {'sort' : {'_id' : -1}, 'limit' : 1};
+  var query = {priority : {$gte : min_level, $lt : 5}};
+  var options = {sort : {'_id' : -1}, limit : 1};
   collection.find(query, options, function(err, docs) {
     if (err) {
-      return res.send(JSON.stringify({'message' : err.message}));
+      return res.send(JSON.stringify({message : err.message}));
     }
-    return res.send(JSON.stringify(docs);
+    return res.send(JSON.stringify(docs));
   });
 });
 
 function GetControlDoc(collection, detector, callback) {
-  var query = {'detector' : detector};
+  var query = {detector : detector};
   collection.find(query, function(err, docs) {
     if (err) {
       callback(err.msg, null);
@@ -109,7 +119,23 @@ function GetControlDoc(collection, detector, callback) {
       callback("No control doc for detector " + detector, null);
     }
     callback(null, docs[0]);
-  }
+  });
+}
+
+function GetDetectorStatus(collection, detector, callback) {
+  var now = Math.floor(new Date().getTime()/1000) - timeout;
+  var oid = ObjectId(now.toString(16) + "0000000000000000");
+  var query = {detector : detector, '_id' : {$gte : oid}};
+  var options = {sort : {'_id' : -1}, limit : 1};
+  collection.find(query, function(err, docs) {
+    if (err) {
+      callback(err.message, null);
+    }
+    if (docs.length == 0) {
+      callback("No status update within the last 30 seconds", null);
+    }
+    callback(null, docs[0]);
+  });
 }
 
 router.get("/getcommand/:detector", checkKey, function(req, res) {
@@ -117,7 +143,7 @@ router.get("/getcommand/:detector", checkKey, function(req, res) {
   var collection = req.db.get("detector_control");
   GetControlDoc(collection, detector, function(err, doc) {
     if (err) {
-      return res.send(JSON.stringify({'message' : err}));
+      return res.send(JSON.stringify({message : err}));
     }
     return res.send(JSON.stringify(doc));
   });
@@ -126,50 +152,101 @@ router.get("/getcommand/:detector", checkKey, function(req, res) {
 router.get("/detector_status/:detector", checkKey, function(req, res) {
   var detector = req.params.detector;
   var timeout = 30;
-  var now = Math.floor(new Date().getTime()/1000) - timeout;
-  var oid = ObjectId(now.toString(16) + "0000000000000000");
-  var query = {'detector' : detector, '_id' : {'$gte' : oid}};
-  var options = {'sort' : {'_id' : -1}, 'limit' : 1};
   var collection = req.db.get("aggregate_status");
-  collection.find(query, options, function(err, docs) {
+  GetDetectorStatus(collection, detector, function(err, doc) {
     if (err) {
-      return res.send(JSON.stringify({'message' : err.message}));
+      return res.send(JSON.stringify({'message' : err}));
     }
-    if (docs.length == 0) {
-      return res.send({'message' : 'No status update within the last 30 seconds'});
-    }
-    return res.send(JSON.stringify(docs));
+    return res.send(JSON.stringify(doc));
   });
 });
 
 router.post("/setcommand/:detector", checkKey, function(req, res) {
+  var q = url.parse(req.url, true).query;
+  var user = q.user;
   var data = req.body.data;
   var detector = req.params.detector;
-  GetControlDoc(detector, function(err, doc) {
+  var ctrl_coll = req.db.get("detector_control");
+  GetControlDoc(ctrl_coll, detector, function(err, doc) {
     if (err) {
-      return res.send(JSON.stringify({'message' : err}));
+      return res.send(JSON.stringify({message : err}));
     }
+    // first - is the detector in "remote" mode?
     if (doc.remote == "false") {
-      return res.send(JSON.stringify({'message' :
+      return res.send(JSON.stringify({message :
         "Detector must be in remote mode to control via the API"}));
     }
-    if ((doc.active == "true" && data.active == "true") || 
-        (doc.active == "false" && data.active == "false")) {
-      
+    // is the detector startable?
+    if (data.active == "true" && doc.active != "false") {
+      return res.send(JSON.stringify({message : "Detector must be stopped first"}));
     }
-    if (detector == "tpc" && (doc.link_nv != "false" || doc.link_mv != "false")) {
-      return res.send(JSON.stringify({'message' :
-        'All detectors must be unlinked to start TPC'}));
+    // is the detector stoppable?
+    if (data.active == "false" && doc.active != "true") {
+      return res.send(JSON.stringify({message : "Detector must be running to stop it"}));
     }
-    if (detector == "neutron_veto" && doc.link_nv != "false") {
-      return res.send(JSON.stringify({'message' :
-        'NV must be unlinked to control via API'}));
-    }
-    if (detector == "muon_veto" && doc.link_mv != "false") {
-      return res.send(JSON.stringify({'message' :
-        'MV must be unlinked to control via API'}));
-    }
-  });
+    // check linking status
+    GetControlDoc(ctrl_coll, 'tpc', function(errtpc, tpc) {
+      if (errtpc) {
+        return res.send(JSON.stringify({message : errtpc}));
+      }
+      if (detector == "tpc" && (tpc.link_nv != "false" || tpc.link_mv != "false")) {
+        return res.send(JSON.stringify({message :
+          'All detectors must be unlinked to start TPC via API'}));
+      }
+      if (detector == "neutron_veto" && tpc.link_nv != "false") {
+        return res.send(JSON.stringify({message :
+          'NV must be unlinked to control via API'}));
+      }
+      if (detector == "muon_veto" && tpc.link_mv != "false") {
+        return res.send(JSON.stringify({message :
+          'MV must be unlinked to control via API'}));
+      }
+      // now we check the detector status
+      var agg_coll = req.db.get("aggregate_status");
+      GetDetectorStatus(agg_coll, detector, function(errstat, status_doc) {
+        if (errstat) {
+          return res.send(JSON.stringify({message : errstat}));
+        }
+        if (status_doc.status != 0 && data.active != "false") {
+          return res.send(JSON.stringify({message : "Detector " + detector + 
+            " must be IDLE (0) but is " + status_enum[status_doc.status] + 
+            " (" + status_doc.status + ")"}));
+        }
+        // now we validate the incoming command
+        var update_doc = {};
+        var query = {detector : detector};
+        var options = {};
+        if (data.active == "false") {
+          ctrl_coll.updateOne({detector : detector},
+            {$set : {active : 'false', user : user}}, options,
+            function(err, result) {
+              if (err) return res.send(JSON.stringify({message : err.message}));
+              return res.send(JSON.stringify({message : 'Update successful'}));
+            });
+        } else {
+          var options_coll = req.db.get("options");
+          options_coll.countDocuments({name : data.mode}, options, function(err, result) {
+            if (result) return res.send(JSON.stringify({message : err.message}));
+            if (result == 0) return res.send(JSON.stringify({message : 'No options document named ' + data.mode}));
+            // FINALLY we can tell the system to do something
+            // now that the user input is sanitized to ISO-5
+            var update = {mode : data.mode, user : user,
+                          link_nv : "false", link_mv : "false"};
+            if (typeof data.comment != 'undefined') update.comment = data.comment;
+            if (typeof data.stop_after != 'undefined') {
+              try {
+                update.stop_after = parseInt(data.stop_after);
+            } catch () {}
+            ctrl_coll.updateOne({detector : detector}, {$set : update}, options,
+                function(err, result) {
+                  if (err) return res.send(JSON.stringify({message : err.message}));
+                  return res.send(JSON.stringify({message : 'Update successful'}));
+            });
+          }); // count options docs
+        } // data.active
+      }); // get status
+    }); // get tpc
+  }); // get detector
 });
 
 module.exports = router;
