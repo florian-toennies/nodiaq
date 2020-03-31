b21ce21c005328e5001f496ab4641212366567cf
Darryl Masson <darrylmasson@gmail.com>
2020-03-31 13:29:36 +0200

    Ready for testing

--- public/javascripts/scope_script.js
+++ public/javascripts/scope_script.js
@@ -1,73 +1,113 @@
 function PopulateAvailableRuns(divname){
     $.getJSON('scope/available_runs', function(data){
-	html = "";
+      console.log("FILLRUNS");
+      console.log(data);
+        if (typeof data.message != 'undefined') {
+            alert(data.message);
+            return;
+        }
+        var html = "";
+        if (data.length == 0) {
+            alert("No runs available");
+            return;
+        }
 	for(var i in data){
-	    // for now	   
 	    html += "<option value="+data[i]+">"+data[i]+"</option>";
 	}
 	document.getElementById(divname).innerHTML = html;
     });
 }
 
-function FillChunks(){
-  var run = $('#run_select').val();
-  $.getJSON('scope/available_chunks?run='+run, function(data){
-    var html = '';
-    for (var i in data) {
-      html += "<option value="+data[i]+">"+data[i]+"</option>";
+function FillTargets() {
+    var run = $("#run_select").val();
+    $.getJSON('scope/available_targets?run=' + run, function(data) {
+      console.log("FILLTARGETS");
+      console.log(data);
+        if (typeof data.message != 'undefined') {
+            alert(data.message);
+            return;
+        }
+        var html = "";
+        if (data.length == 0) {
+            alert("No data types available");
+            return;
+        }
+        for (var i in data) {
+            html += "<option value=" + data[i] + ">" + data[i] + "</option>";
+        }
+        $("#target_select").html(html);
     }
-    $("#chunk_select").html(html);
-  });
-}
-
-function FillThreads(){
-  var run = $('#run_select').val();
-  var chunk = $('#chunk_select').val();
-  var channel = $("#channel_select").val();
-  $.getJSON("scope/available_threads?run="+run+"&channel="+channel.toString()+"&chunk="+chunk, function(data) {
-    var html = '';
-    for (var i in data) {
-      html += "<option value="+data[i]+">"+data[i]+"</option>";
-    }
-    $("#thread_select").html(html);
-  });
 }
 
 function GetData(){
 
-    var run = $("#run_select").val();
+    var run = (''+$("#run_select").val()).padStart(6, '0');
+    var target = $("#target_select").val();
+    var max_n = $("#num_select").val();
     var channel = $("#channel_select").val();
-    var chunk=$("#chunk_select").val();
-    var thread=$("#thread_select").val();
 
-    $.getJSON('scope/get_pulses?run='+run+'&channel='+channel.toString()+"&chunk="+chunk+"&thread="+thread, function(data){
-        if (typeof data.message != 'undefined') {
-          alert(data.message);
+    if (run.search(/[0-9]{6}/) == -1) {
+        alert("That isn't a valid run id");
+        return;
+    }
+    if (max_n > 1000) {
+        alert("You can't ask for that many entries");
+        return;
+    }
+
+    var url = "http://eb2:8888/get_data";
+    var query = "?run_id="+run+"&target="+target+"&max_n="+max_n+"&selection_str=channel=="+channel;
+
+    $.getJSON(url+query, function(data){
+        if (typeof data.error != 'undefined') {
+            alert("Microstrax sent an error: " + data.error);
+            return;
+        }
+        if (data.length == 0) {
+          alert("No data with those selections");
           return;
         }
-	document.data = data;
-	document.index=0;
+        document.data = data;
+        document.index=0;
+        var tab_head_html = "<tr><th>Index</th><th>Time (strax)</th><th>Time (human-readable)</th>";
+        for (var key in data[0]) {
+            if (key === 'data' || key === 'time') continue;
+            tab_head_html += "<th>"+key.charAt(0).toUpperCase()+key.slice(1)+"</th>";
+        }
+        tab_head_html += "</tr>";
+        $("#scopetab_head").html(tab_head_html);
 
-	table_html = "";
-	for(var i in data){
+	    var table_html = "";
+	    for(var i in data){
             var frag = data[i];
-	    var minutes = (Math.floor(frag['time'] / (1e9*60))).toFixed(0);
-	    var seconds = (Math.floor((frag['time']/1e9) % 60)).toFixed(0);
-	    var millis = (frag['time']/1e6 - 
-			  ((minutes*60*1000)+seconds*1000)).toFixed(0);
-	    var human_time = minutes.padStart(2, '0') + ":" + 
-		seconds.padStart(2, '0') + ":"+millis.padStart(3, '0')+
-		" into run";
-	    table_html += "<tr id='"+i.toString()+"_row'><td>" + i.toString() + "</td><td>" +
-		frag['channel'] + "</td><td>" + frag['time'] + 
-		"</td><td>" + human_time + "</td><td>" + frag['pulse_length'] + "</td></tr>";
-	}
-	document.getElementById("pulse_table").innerHTML=table_html;
+            var ts = frag['time'];
+            var d = new Date(ts*1e-6);
+            var ns = ts % 1000;
+            var us = (Math.floor((ts % 1000000)/1000)).toFixed(0);
+            var human_time = d.getUTCFullYear() + '-';
+            human_time += d.getUTCMonth().padStart(2, '0' + '-';
+            human_time += d.getUTCDate().padStart(2, '0' + ' ';
+            human_time += d.getUTCHours().padStart(2, '0' + ':';
+            human_time += d.getUTCMinutes().padStart(2, '0' + ':';
+            human_time += d.getUTCSeconds().padStart(2, '0') + '.';
+            human_time += d.getUTCMilliseconds().padStart(3, '0') + ' ';
+            human_time += us.padStart(3, '0') + ' ';
+            human_time += ns.padStart(3, '0');
+            var row_html = "<tr id='"+i.toString()+"_row'><td>" + i.toString() + "</td>";
+            row_html += "<td>" + ts + "</td><td>" + human_time + "</td><td>";
+            for (var key in frag) {
+                if (key === 'time' || key === 'data') continue;
+	            row_html += "<td>" + frag[key].toString() + "</td>";
+            }
+            row_html += "</tr>";
+            table_html += row_html;
+	    } // var i in data
+	    document.getElementById("scopetab_body").innerHTML=table_html;
 
-	if(document.chart != null)
-	    document.chart.destroy();
-	DrawChart();
-    });
+	    if(document.chart != null)
+	        document.chart.destroy();
+	    DrawChart();
+    }); // getJSON
 }
 
 function Next(){
@@ -85,23 +125,19 @@ function Prev(){
 }
 
 function DrawChart(){
-    dat = [];
-    for(var i in document.data[document.index]['sample'])
-	dat.push([parseInt(i), parseInt(document.data[document.index]['sample'][i])]);
-    console.log(dat);
-    document.getElementById("channel_id").innerHTML = document.data[document.index]['channel'];
-    document.getElementById("time_id").innerHTML = document.data[document.index]['time'];
-    document.getElementById("length_id").innerHTML = document.data[document.index]['pulse_length'];
+    var dat = [];
+    for(var i in document.data[document.index]['data'])
+	    dat.push([parseInt(i), parseInt(document.data[document.index]['data'][i])]);
+    //console.log(dat);
 
-    for(var i =0; i<50; i+=1){
-	if(i != document.index){
-	    $("#"+i.toString()+"_row").css('background-color', 'white');
-	    $("#"+i.toString()+"_row").css('color', 'black');
-	}
-	else{
-	    $("#"+i.toString()+"_row").css('background-color', '#ef476f');
-	    $("#"+i.toString()+"_row").css('color', 'white');
-	}
+    for(var i =0; i<document.data.length; i+=1){
+	    if(i != document.index){
+	        $("#"+i.toString()+"_row").css('background-color', 'white');
+	        $("#"+i.toString()+"_row").css('color', 'black');
+	    }else{
+	        $("#"+i.toString()+"_row").css('background-color', '#ef476f');
+	        $("#"+i.toString()+"_row").css('color', 'white');
+	    }
     }
 
     document.chart = new Dygraph(
@@ -112,10 +148,10 @@ function DrawChart(){
 	dat,
 
 	{
-	    "xlabel": "Sample in pulse [10ns]",
-	    "ylabel": "Voltage [ADC Units]",
+	    "xlabel": "Sample in pulse [units of dt]",
+	    "ylabel": "Amplitude [depends]",
 	    "yLabelWidth": 18,
-	    "labels": ["Sample", "Voltage"],
+	    "labels": ["Sample", "Amplitude"],
 	}
     );
 	
--- routes/scope.js
+++ routes/scope.js
@@ -5,11 +5,7 @@
 var express = require("express");
 var url = require("url");
 var router = express.Router();
-var fs = require('fs');
-var lz4 = require('lz4');
 var gp = '';
-
-var runs_fs_base = '/live_data/xenonnt';
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) { return next(); }
@@ -21,200 +17,40 @@ router.get('/', ensureAuthenticated, function(req, res) {
 });
 
 router.get('/available_runs', ensureAuthenticated, function(req, res){
-    var fspath = runs_fs_base;
-    fs.readdir(fspath, function(err, items) {
-      if (err) return res.send(JSON.stringify({message : err.message}));
-      items = items.sort(function(a,b) {return parseInt(b)-parseInt(a);});
-      return res.send(JSON.stringify(items));
-    });
+  var collection = req.runs_db.get(process.env.RUNS_MONGO_COLLECTION);
+  var query = {'data.type' : /eb[0-5]\.xenon\.local/};
+  var options = {};
+  collection.distinct('number', query, options, function(err, docs) {
+    if (err) return res.send(JSON.stringify({message : err.message}));
+    return res.send(JSON.stringify(docs));
+  }); // collection.find
 });
 
-function ReadFragments(path, thechannel, max_fragments){
-    // Get strax data (up to max_fragments) for a given module and channel 
-    // at a given path
-    file_buffer = fs.readFileSync(path);
-    console.log("Reading frament at " + path);
-    var uncompressed = new Buffer(file_buffer.length*3); // it can't do 3x compression, right?
-    var uncompressedSize = lz4.decodeBlock(file_buffer, uncompressed);
-    var data = uncompressed.slice(0, uncompressedSize);
-    var i = 0;
-    var pulses = [];
-    return new Promise( resolve => {
-	while(i+141 < data.length && pulses.length<max_fragments && i>=0){
-
-	    // Get header data for first fragment in pulse
-	    var channel = data.slice(i, i+2).readInt16LE();	    
-	    var time = parseInt(data.slice(i+4, i+12).reverse().toString('hex', 0, 8), 16);
-	    var frag_length = data.slice(i+12, i+16).readInt32LE();
-	    var pulse_length = data.slice(i+20, i+24).readInt32LE();
-	    
-	    var samples = [];
-	    for(var j=0; j<frag_length; j+=1){
-		if(samples.length > pulse_length)
-		    break;
-		samples.push(data.slice(i+31+2*j, i+31+2*j+2).readInt16LE());
-	    }
-
-	    // Now get further fragments if they exist
-	    i += frag_length*2 + 31;
-	    
-	    while(samples.length < pulse_length){
-		
-		for(var j=0; j<frag_length; j+=1){
-		    if(samples.length > pulse_length)
-			break;
-		    samples.push(data.slice(i+31+j*2, i+31+j*2+2).readInt16LE());
-		}
-		i+= frag_length*2+31;
-		
-	    }
-
-	    if(channel === thechannel)
-		pulses.push({"channel": channel, "time": time, "pulse_length": pulse_length, 
-			     "sample": samples});
-	    
-	}
-	resolve(pulses);
-    });
-}
-
-
-
-
-// This construct is probably unreasonably complex, but it has worked before.
-// We declare a recursive function returning a promise, whose result is awaited
-// by an async wrapper function, whose callback actually returns our JSON, the 
-// invocation of which is nested within the callback of a database query.
-// Don't lie. You love it.
-function PullData(reader, channel, full_path, ret_pulses, n_pulses,
-		  last_file_called, last_chunk_called){
-    // General logic: for this chunk list all sub-files available from out reader in 
-    // alphabetical order. Keep track of most recent files read. Try to pull n_pulses 
-    // pulses from the current file and add them to ret_pulses. If you can't, call this
-    // function again on the previous file in chunk. If at first file of chunk iterate 
-    // chunk number once down. If already at chunk zero just return what you've got.
-
-    // Get all the sub-files in full_path that start with 'reader' and sort by name
-    // Chunk names have to be of length 6
-    var s = "000000" + last_chunk_called;
-    var chunk_name = s.substr(s.length-6);
-
-    return new Promise( resolve => {
-	console.log("Reading chunk at " + chunk_name);
-	var items = fs.readdirSync(full_path + "/" + chunk_name);
-	
-	var sorted_files = items.filter(function (file) {
-	    return file.substr(0, reader.length) === reader ? true : false;
-        }).sort(function (a, b) {
-	    return a < b ? -1 : 1;
-        });
-	
-	var index = -1;
-	if(last_file_called === "")
-	    index = sorted_files.length - 1;
-	else{
-	    for(var i in sorted_files){
-		if(sorted_files[i] == last_file_called){		    
-		    index = i-1;
-		    break;
-		}
-	    }
-	    if(index === 0 && last_chunk_called > 0)
-		resolve(PullData(reader, channel, full_path, ret_pulses, n_pulses,
-				"", last_chunk_called - 1));
-	    else if(index === 0 && last_chunk_called === 0){
-		console.log("NO MORE DATA");
-		resolve(ret_pulses); // no more data
-	    }
-	}
-	
-	if(index === -1) // unknown failure. return what you have
-	    resolve(ret_pulses);
-	
-	// Otherwise just read the file. This is actually the hard part tbh. 
-	ReadFragments(full_path + "/" + chunk_name + "/" + sorted_files[index],
-		      channel, n_pulses - ret_pulses.length).then(
-			  result => {
-			      ret_pulses = ret_pulses.concat(result);
-			      if(ret_pulses.length >= n_pulses){		
-				  resolve(ret_pulses);
-			      }
-			      else{
-				  resolve(PullData(reader, channel, full_path, ret_pulses, n_pulses, 
-						  sorted_files[index], last_chunk_called));
-			      }
-			  });
-    }); // end promise
- 
-}
-
-async function GetChannelWaveforms(run, reader, channel, fspath, callback){
-    // This is a wrapper to allow me to await the PullData function. It also
-    // does some initial filesystem searching to get the most recent chunk
-
-    var ret_pulses = [];
-    var last_file_called = "";
-    var last_chunk_called = -1;
-    var n_pulses = 50;
-
-    // Go into the filesystem and get the largest chunk present. For simplicity we ignore
-    // pre and post chunks by only taking chunks with file name length 6.
-    var full_path = fspath + '/' + run;
-    console.log("Searching path ", full_path);
-    items = fs.readdirSync(full_path);
-    var sorted_files = items.filter(function (file) {
-	return file.length === 6 ? true : false;
-    }).sort(function (a, b) {
-	return a < b ? -1 : 1;
-    });
-
-    if(sorted_files.length === 0)
-	callback([]);
-
-    last_chunk_called = parseInt(sorted_files[sorted_files.length - 1]);
-
-    var pulses =  await PullData(reader, channel, full_path, ret_pulses, n_pulses, 
-				 last_file_called, last_chunk_called);
-    callback(pulses);
-
-}
-
-router.get('/_get_pulses', ensureAuthenticated, function(req, res){
-
-    // Get pulses from fspath. 
-    var db = req.db;
-    var options_coll = db.get('options');
-    var board_map_coll = db.get('board_map');
-    var cable_map_coll = db.get('cable_map');
-    var fspath = runs_fs_base;
+router.get("/available_targets", ensureAuthenticated, function(req, res) {
     var q = url.parse(req.url, true).query;
     var run = q.run;
-    var channel = parseInt(q.channel);
-    if(typeof channel === 'undefined' || !(channel > 0))
-	channel = 0;
-    if(typeof run === 'undefined')
-	return JSON.stringify({"error": "no run provided"});
-
-    cable_map_coll.find({'pmt' : channel}, function(e, docs) {
-      if (e)
-        return res.send(JSON.stringify({'error' : e.message}));
-      if (docs.length == 0)
-        return res.send(JSON.stringify({"error" : "Channel not found"}));
-      var board = docs[0]["board"];
-      var adc_channel = docs[0]["adc_channel"];
-      board_map_coll.find({"board" : board}, function(ee, docss) {
-        if (ee)
-          return res.send(JSON.stringify({'error' : ee.message}));
-        if (docss.length == 0)
-          return res.send(JSON.stringify({"error" : "Board not found"}));
-        var reader = docss[0]["host"];
-
-	console.log("Getting channel waveforms at " + fspath);
-	GetChannelWaveforms(run, reader, adc_channel, fspath, function(pulses){
-	  return res.send(JSON.stringify(pulses));
-	});
-      }); // board_map callback
-    }); // cable_map callback
+    if (typeof run === 'undefined') return res.send(JSON.stringify({message : 'Invalid run'}));
+    var collection = req.runs_db.get(process.env.RUNS_MONGO_COLLECTION);
+    try {
+      var query = {number : NumberInt(run)};
+    }catch(err){
+      return res.send(JSON.stringify({message : err.message}));
+    }
+    var options = {
+        'projection' : {'data' : 1}
+    };
+    collection.findOne(query, options, function(err, docs) {
+      if (err) return res.send(JSON.stringify({message : err.message}));
+      if (docs.length == 0) return res.send(JSON.stringify({message : 'No run found'}));
+      var ret = [];
+      for (var i in docs[0]['data']) {
+        var datadoc = docs[0]['data'][i];
+        if (datadoc['host'] == 'rucio-catalog' || datadoc['type'] == 'live')
+          continue;
+        ret.push(datadoc['type']);
+      }
+      return res.send(JSON.stringify(ret));
+    });
 });
 
 router.get("/available_chunks", ensureAuthenticated, function(req, res) {
@@ -228,119 +64,6 @@ router.get("/available_chunks", ensureAuthenticated, function(req, res) {
                    .sort(function(a,b) {return parseInt(b)-parseInt(a);});
       return res.send(JSON.stringify(items));
     });
-});
-
-function GetReader(channel, cable_map_coll, board_map_coll, callback) {
-  cable_map_coll.find({'pmt' : parseInt(channel)}, function(e, docs) {
-    if (e)
-      callback(-1);
-    if (docs.length == 0)
-      callback(-2);
-    board_map_coll.find({"board" : board}, function(ee, docss) {
-      if (ee)
-        callback(-1);
-      if (docss.length == 0)
-        callback(-2);
-      var reader_id = docss[0]["host"][6]; // reader[i]
-    }); // board_map
-  }); // cable_map
-}
-
-router.get('/available_threads', ensureAuthenticated, function(req, res) {
-  var db = req.db;
-  var q = url.parse(req.url, true).query;
-  var run = q.run;
-  var channel;
-  try{
-    channel = q.channel;
-  }catch(error){
-    return res.send(JSON.stringify({error : 'Invalid channel'}));
-  }
-  var chunk = q.chunk;
-  var board_map_coll = db.get('board_map');
-  var cable_map_coll = db.get('cable_map');
-  if (typeof run === 'undefined' || typeof channel === 'undefined' || typeof chunk === 'undefined')
-    return res.send(JSON.stringify({message : 'Undefined input'}));
-  var fspath=runs_fs_base + '/' + run + '/' + chunk;
-  GetReader(channel, cable_map_coll, board_map_coll, function(reader_id) {
-      var threads = files.filter(function(fn) {return fn[6] == reader_id;})
-                         .map(function(fn){return fn.slice(17);});
-      return res.send(JSON.stringify(threads));
-    }); // readdir
-  }); // GetReader
-});
-
-router.get('/get_pulses', ensureAuthenticated, function(req, res) {
-  var db = req.db;
-  var q = url.parse(req.url, true).query;
-  var run = q.run;
-  var chunk = q.chunk;
-  var thread = q.thread;
-  var channel = q.channel;
-  if (typeof run === 'undefined' || typeof chunk === 'undefined' || typeof thread === 'undefiend' || typeof channel === 'undefined')
-    return res.send(JSON.stringify({message : 'Undefined input'}));
-  channel = parseInt(channel);
-  GetReader(channel, db.get('cable_map'), db.get('board_map'), function(reader) {
-    if (reader == -1 || reader == -2)
-      return res.send(JSON.stringify({message : 'Invalid input'}));
-    var filepath = runs_fs_base + '/' + run + '/' + chunk + '/reader' + reader + '_reader_0_' + thread;
-    fs.readFile(filepath, function(err, data) {
-      if (err)
-        return res.send(JSON.stringify({message : err.message}));
-      var decompressed = Buffer.alloc(data.length*3);
-      try{
-        lz4.decodeBlock(data, decompressed);
-      }catch(error){
-        return res.send(JSON.stringify({message : "Caught error: " + error.message}));
-      }
-      var retpulses = [];
-      var idx = 0;
-      const strax_header_size=24;
-      while (idx < decompressed.length) {
-        var frag_idx = 0;
-        var frag_channel = decompressed.readInt16LE(idx+frag_idx);
-        console.log("This frag is channel " + decompressed.readInt16LE(idx+frag_idx));
-        frag_idx += 2;
-        var frag_dt = decompressed.readInt16LE(idx+frag_idx);
-        frag_idx += 2;
-        var frag_time_msb = decompressed.readInt32LE(idx+frag_idx);
-        frag_idx += 4;
-        var frag_time_lsb = decompressed.readInt32LE(idx+frag_idx);
-        frag_idx += 4;
-        var frag_time_msb = decompressed.readInt32LE(idx+frag_idx);
-        frag_idx += 4;
-        var frag_time = parseInt(frag_time_msb.toString(16) + frag_time_lsb.toString(16), 16);
-        //var frag_time = decompressed.readBigInt64LE(idx+frag_idx);
-        //frag_idx += 8; // node version too old
-        // can't bitshift because js is 32-bit trash
-        var pulse_length = decompressed.readInt32LE(idx+frag_idx);
-        frag_idx += 4;
-        var frag_dt = decompressed.readInt16LE(idx+frag_idx);
-        frag_idx += 2;
-        var frag_channel = decompressed.readInt16LE(idx+frag_idx);
-        console.log("This frag is channel " + frag_channel);
-        frag_idx += 2;
-        var frag_length = decompressed.readInt32LE(idx+frag_idx);
-        console.log("This frag is " + frag_length + " samples long");
-        var frag_i = decompressed.readInt16LE(idx+frag_idx);
-        frag_idx += 2;
-        var baseline = decompressed.readInt16LE(idx+frag_idx);
-        frag_idx += 4;
-        if (frag_channel != channel) {
-          idx += strax_header_size;
-          idx += frag_length*2;
-          continue;
-        }
-        wf = [];
-        for (; frag_idx < strax_header_size + frag_length*2; frag_idx += 2)
-          wf.push(decompressed.readInt16LE(idx+frag_idx));
-        retpulses.push({time: frag_time, pulse_length: pulse_length, frag_i: frag_i,
-                        sample: wf, channel: channel});
-        idx += frag_idx;
-      } // while in chunk
-      return res.send(JSON.stringify(retpulses));
-    }); // fs.readfile
-  }); // getreader
 });
 
 module.exports = router;
